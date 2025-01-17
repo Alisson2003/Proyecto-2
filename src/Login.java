233f1609e45bc6c456cfa0fcbfed6d4190c39093
Alisson2003 <alissobn.viracocha@epn.edu.ec>
2025-01-17 00:26:01 -0500

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Login {
+    public JPanel mainPanel;
+    private JButton cancelarButton;
+    private JButton accederButton;
+    private JTextField usuario;
+    private JPasswordField passwordField1;
+
+
+}
